8db31d1021c444a4814c5b515ac83475ec078762
mire-web <amahe8664@gmail.com>
2024-08-15 23:29:49 +0100

    str1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Returns the concatenation of two strings typed
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Returns concatenation of str1 and str2 """
+    return str1 + str2
