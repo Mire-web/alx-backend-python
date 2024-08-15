ebd1fdb7cbe0378cf3b99cdc2f921d85d5d5f5d3
mire-web <amahe8664@gmail.com>
2024-08-16 00:42:25 +0100

    kv2

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,6 +5,6 @@ Return a tuple containing str and squared int or float
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
-    """ Store result from parameter in a tuple """
-    return (k, float(v) ** 2)
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ Return a tuple """
+    return (k, float(v ** 2))
