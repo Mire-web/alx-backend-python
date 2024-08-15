dd586fa81699d61d87ef87ff0398e7df01c1d9af
mire-web <amahe8664@gmail.com>
2024-08-15 23:42:52 +0100

    floor2

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -5,13 +5,8 @@ Returns the floor of any float its given
 
 
 def floor(n: float) -> int:
+    """ return the floor of a function """
     floored = 0
     for i in range(int(n)):
         floored = i
     return i + 1
-
-if __name__ == '__main__':
-    print(floor(3.54))
-    print(floor(4.333))
-    print(floor(5.322))
-    print(floor(35.33))
