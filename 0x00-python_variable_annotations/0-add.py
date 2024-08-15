6e00b5c4751155e264785f28a70c20d245c29dbe
mire-web <amahe8664@gmail.com>
2024-08-15 23:26:03 +0100

    float2

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Annotated function to add two floating numbers and return the sum
 """
 
 
 def add(a: float, b: float) -> float:
+    """ Return the sum of two float numbers """
     return a + b
