8f9ae46809542ec23b4a8b6784181011f67b4786
mire-web <amahe8664@gmail.com>
2024-08-19 19:35:05 +0100

    done

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ Return Sorteed list """
     async_list = [wait_random(max_delay) for _ in range(n)]
     sorted_list = [await task for task in asyncio.as_completed((async_list))]
     return sorted_list
