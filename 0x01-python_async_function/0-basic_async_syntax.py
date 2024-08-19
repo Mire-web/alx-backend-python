87cf3e30cb0ffac311d0247a03922b61dc006e6e
mire-web <amahe8664@gmail.com>
2024-08-19 19:34:13 +0100

    concurrent-run

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,6 +7,7 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """ Simulate a asynchronous operation """
     result = random.uniform(0, max_delay)
     await asyncio.sleep(result)
     return result
---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Run concurrent Coroutines at the same time
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    async_list = [wait_random(max_delay) for _ in range(n)]
+    sorted_list = [await task for task in asyncio.as_completed((async_list))]
+    return sorted_list
