30b4d0c5f35ba020b171858968a412325bf6b47a
mire-web <amahe8664@gmail.com>
2024-08-20 17:37:49 +0100

    0

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Comprehensively collect 10 random outputs from async_generator
+"""
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    return [i async for i in async_generator()]
