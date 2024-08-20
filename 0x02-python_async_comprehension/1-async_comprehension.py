181518e451c5cad54d07a76e47f08baa65a1d8fe
mire-web <amahe8664@gmail.com>
2024-08-20 17:42:03 +0100

    async-gen-comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,4 +7,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension():
+    """ Return a comprehensive list of generated random numbers """
     return [i async for i in async_generator()]
